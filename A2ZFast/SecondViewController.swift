3c26d1023aa8647c79857374d4eca757ccf7d830
Yogesh Hande <yogeshHande@macintosh.local>
2015-12-23 23:20:43 -0500

    Initial Commit

---
+++ A2ZFast/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  A2ZFast
+//
+//  Created by Yogesh Hande on 12/23/15.
+//  Copyright © 2015 SayaliYogesh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
